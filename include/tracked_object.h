1db9c60aaa571395ddd40a4bbdb9b63497af1c3b
Aref Azizpour <aref.azizpour@gmail.com>
2020-07-28 01:26:29 +0430

    redundant files removed

--- include/tracked_object.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by aref on 7/24/20.
-//
-
-#ifndef TRACKING_TRACKED_OBJECT_H
-#define TRACKING_TRACKED_OBJECT_H
-
-#endif //TRACKING_TRACKED_OBJECT_H
--- src/tracked_object.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by aref on 7/24/20.
-//
-
-#include "tracked_object.h"
